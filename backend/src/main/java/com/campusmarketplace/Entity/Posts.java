e6f9bb9e366b6617e2f821519b4d204f7a33bb13
Rashid-Abdi <abdirashidabdi04@gmail.com>
2025-10-14 22:31:22 -0500

    initialized posts entity class

---
+++ backend/src/main/java/com/campusmarketplace/Entity/Posts.java
@@ -0,0 +1,25 @@
+package com.campusmarketplace.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private Double price;
+    private String category;
+    private String imageUrl;
+}
